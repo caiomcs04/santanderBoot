835496055be41fcb1de05cdcc5911ed8fd6879c7
caiomcs04 <caio6c@yahoo.com.br>
2021-05-28 18:37:25 -0300

    Detalhes para o Swagger

--- santanderBoot/src/main/java/com/project/santanderBoot/SantanderBootApplication.java
+++ santanderBoot/src/main/java/com/project/santanderBoot/SantanderBootApplication.java
@@ -3,6 +3,7 @@ package com.project.santanderBoot;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -17,9 +18,9 @@ public class SantanderBootApplication {
 	}
 
 	@Bean
-	public OpenAPI customOpenAPI(){
+	public OpenAPI customOpenAPI(@Value("${application.description}") String description){
 		return new OpenAPI().info(new Info()
-				.title("")
+				.title(description)
 				.version("1.0")
 				.termsOfService("http://swagger.io/terms")
 				.license(new License().name("Apache 2.0").url("http://springdoc.org")));
